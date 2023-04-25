0d1f761557f4cec9a8e59819b18038adbbdd1aa8
Harsh Upadhyay <upadhyay.harsh1988@gmail.com>
2023-04-25 13:09:55 -0700

    fix date range query

--- mongoConnector/src/main/java/com/harsh/microSvc/mongoConnector/controller/SalesRestController.java
+++ mongoConnector/src/main/java/com/harsh/microSvc/mongoConnector/controller/SalesRestController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.harsh.microSvc.mongoConnector.controller;
 
@@ -8,6 +8,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.datetime.DateFormatter;
@@ -33,65 +35,67 @@ import com.harsh.microSvc.mongoConnector.service.SalesService;
 @RestController
 @RequestMapping("/sales")
 public class SalesRestController {
-	@Autowired
-	private SalesService salesService;
-	
-	// CREATE
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Sale addSale(@RequestBody Sale sale) {
-		return salesService.addSale(sale);
-	}
+    @Autowired
+    private SalesService salesService;
+    private static final Logger _logger = Logger.getLogger(SalesRestController.class.getName());
 
-	// READ
-	@GetMapping
-	public List<Sale> getAllSales() {
-		return salesService.findAllSales();
-	}
+    // CREATE
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sale addSale(@RequestBody Sale sale) {
+        return salesService.addSale(sale);
+    }
 
-	@GetMapping("/{id}")
-	public Optional<Sale> getSale(@PathVariable String id) {
-		return salesService.getSaleById(id);
-	}
+    // READ
+    @GetMapping
+    public List<Sale> getAllSales() {
+        return salesService.findAllSales();
+    }
 
-	@GetMapping("/storeLocation/{storeLocation}")
-	public List<Sale> getSalesByStoreLocation(@PathVariable String storeLocation) {
-		return salesService.getSaleByStoreLocation(storeLocation);
-	}
+    @GetMapping("/{id}")
+    public Optional<Sale> getSale(@PathVariable String id) {
+        return salesService.getSaleById(id);
+    }
 
-	@GetMapping("/couponUsed/{couponUsed}")
-	public List<Sale> getSalessByCouponUsed(@PathVariable Boolean couponUsed) {
-		return salesService.getSalesByCouponUsed(couponUsed);
-	}
+    @GetMapping("/storeLocation/{storeLocation}")
+    public List<Sale> getSalesByStoreLocation(@PathVariable String storeLocation) {
+        return salesService.getSaleByStoreLocation(storeLocation);
+    }
 
-	@GetMapping("/saleDateBetween")
-	public List<Sale> getSalesByDateBetween(@RequestParam(required=false) String from, @RequestParam(required=false) String to) {
-		Date end, start;
-		DateFormatter df = new DateFormatter();
-		try {
-			end = df.parse(to, Locale.US);
-		} catch (Exception e){
-			end = new Date();
-		}
-		
-		try {
-			start = df.parse(from, Locale.US);
-		} catch (Exception e){
-			start = Date.from(end.toInstant().minus(24, ChronoUnit.HOURS));
-		}
-		
-		return salesService.getSalesByDateBetween(start, end);
-	}
+    @GetMapping("/couponUsed/{couponUsed}")
+    public List<Sale> getSalessByCouponUsed(@PathVariable Boolean couponUsed) {
+        return salesService.getSalesByCouponUsed(couponUsed);
+    }
 
-	// UPDATE
-	@PutMapping
-	public Sale updateSale(@RequestBody Sale saleRequest) {
-		return salesService.updateSale(saleRequest);
-	}
-	
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public void deleteSale(@PathVariable String id) {
-		salesService.deleteSale(id);
-	}
+    @GetMapping("/saleDateBetween")
+    public List<Sale> getSalesByDateBetween(@RequestParam(required = false) String from, @RequestParam(required = false) String to) {
+        Date end, start;
+        DateFormatter fmt = new DateFormatter("yyyy-MM-dd HH:mm:ss");
+        try {
+            end = fmt.parse(to, Locale.US);
+        } catch (Exception e) {
+            _logger.log(Level.WARNING, e.getMessage());
+            end = new Date();
+        }
+
+        try {
+            start = fmt.parse(from, Locale.US);
+        } catch (Exception e) {
+            _logger.log(Level.WARNING, e.getMessage());
+            start = Date.from(end.toInstant().minus(24, ChronoUnit.HOURS));
+        }
+        return salesService.getSalesByDateBetween(start, end);
+    }
+
+    // UPDATE
+    @PutMapping
+    public Sale updateSale(@RequestBody Sale saleRequest) {
+        return salesService.updateSale(saleRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteSale(@PathVariable String id) {
+        salesService.deleteSale(id);
+    }
 }
